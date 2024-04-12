efa087fbf38fe60eee902c6462aa024be5c67a52
NCTona <143495151+NCTona@users.noreply.github.com>
2024-04-13 04:24:25 +0700

    init project

---
+++ assets/js/time.js
@@ -0,0 +1,49 @@
+// ┌┬┐┬┌┬┐┌─┐
+//  │ ││││├┤
+//  ┴ ┴┴ ┴└─┘
+// Set time and Date
+
+window.onload = displayClock();
+function displayClock() {
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  var d = new Date();
+  var mm = monthNames[d.getMonth()];
+  var dd = d.getDate();
+  var day = weekday[d.getDay()];
+
+  var min = (mins = ("0" + d.getMinutes()).slice(-2));
+  var hh = d.getHours();
+  var ampm = "";
+
+  document.getElementById("hour").innerText = hh;
+  document.getElementById("separator").innerHTML = " : ";
+  document.getElementById("minutes").innerText = min + ampm;
+
+  document.getElementById("month").innerText = day + ", " + mm;
+  document.getElementById("day").innerText = dd;
+
+  setTimeout(displayClock, 1000);
+}
---
+++ config.js
@@ -0,0 +1,70 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: "Phuoc",
+  imageBackground: false,
+  openInNewTab: false,
+  twelveHourFormat: false,
+
+  // Greetings
+  greetingMorning: "おはよう~ <3",
+  greetingAfternoon: "こんにちは~",
+  greetingEvening: "こんばんは~",
+  greetingNight: "寝てください!!!",
+
+  // Layout
+  bentoLayout: "bento", // 'bento', 'lists', 'buttons', 
+
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+  firstButtonsContainer: [
+    {
+      id: "1",
+      name: "Youtube",
+      icon: "youtube",
+      link: "https://youtube.com/",
+    },
+    {
+      id: "2",
+      name: "Facebook",
+      icon: "facebook",
+      link: "https://facebook.com/",
+    },
+    {
+      id: "3",
+      name: "Reddit",
+      icon: "glasses",
+      link: "https://reddit.com",
+    },
+    {
+      id: "4",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com",
+    },
+    {
+      id: "5",
+      name: "X",
+      icon: "twitter",
+      link: "https://x.com",
+    },
+    {
+      id: "6",
+      name: "Zalo",
+      icon: "message-circle-heart",
+      link: "https://chat.zalo.me/",
+    },
+  ],
+};
