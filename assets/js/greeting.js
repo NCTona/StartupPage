7bb149df2bc0850fff97c896e490dc639e9ffa89
NCTona <143495151+NCTona@users.noreply.github.com>
2024-04-24 14:21:49 +0700

    Update greeting.js

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -12,11 +12,11 @@ const gree2 = `${CONFIG.greetingMorning}\xa0`;
 const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
-if (hour >= 23 || hour < 6) {
+if (hour >= 0 || hour < 6) {
   document.getElementById("greetings").innerText = gree1 ;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById("greetings").innerText = gree2 ;
-} else if (hour >= 12 && hour < 17) {
+} else if (hour >= 12 && hour < 18) {
   document.getElementById("greetings").innerText = gree3 ;
 } else {
   document.getElementById("greetings").innerText = gree4 ;
